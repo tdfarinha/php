1a3ef2b944a187c4023505d53df94d5af883e617
Guilherme Pires <79325677+Guilhermezanoello@users.noreply.github.com>
2024-04-15 19:46:40 +0100

    Mensagem de ERRO

--- paginas/Msg_erro.php
+++ paginas/Msg_erro.php
@@ -1,3 +1,4 @@
+<!DOCTYPE html>
 <html>
 <head>
 <style>
@@ -7,107 +8,94 @@
   }
   
   #erro-box{
-    background-color:#F78181;
-    width:380px;
-    height:200px;
+    background-color:#de5d02;
+    width:800px;
+    height:300px;
     margin: 140px auto 0px;
     overflow:hidden;
-    box-shadow:0px 0px 5px #6F6666;
+    box-shadow:0px 0px 5px #ffffff;
   }
   
   #erro-cabecalho{
-    background-color:red;
+    background-color:#f3ac1e;
     height:50x;
-    border-bottom:2px solid #BDBDBD;
+    border-bottom:2px solid #e5dcd7;
     text-align:center;
     font: bold 20px/50px sans-serif;
-    color: white;
+    color: #ffffff;
   }
   
-  .input-div{
-    margin:20px;
-    padding:5px;
+  .mensagem {
+    margin: 50px auto; /* Centraliza verticalmente */
+    width: 80%; /* Largura ajustável */
+    text-align: center; /* Centraliza horizontalmente */
     font: bold 15px sans-serif;
     color:black;
-   }
+  }
  
-  .input-div input{
-    width:325px;
-    height:35px;
-    padding-left:7px;
-    font: normal 13px sans-serif;
-    color:#0B6121;
-  }
-  #input-pass{
-    margin-top:-15px;
-  }
-  #input-user{
-    margin-top:10px;
-  }
-  
   #acoes{
-    width:330px;
-    margin:25px;
-  }
+    width:100%; /* Largura total */
+    text-align: center; /* Centraliza horizontalmente */
+    margin-top: 20px;
+  } 
   
   #registo{
-    float:left;
-    margin-top:-10px;
+    float: left; /* Alinha à esquerda */
+    margin-left: 20px; /* Adiciona um espaço à esquerda */
   }
   
-  input[type=submit]{
-    float:right;
-    background-color:red;
-    padding: 10px 50px;
-    margin-top:-15px;
+  button {
+    background-color: #f3ac1e;
+    padding: 10px 20px;
     font: bold 13px sans-serif;
-    color:white;
-    box-shadow:2px 2px 5px #6F6666;
-    cursor:pointer;
-    border:0px;
+    color: white;
+    border: 0;
+    cursor: pointer;
+    box-shadow: 2px 2px 5px #ffffff;
+    margin-right: 20px; /* Adiciona um espaço à direita */
   }
   
-  input[type=submit]:hover{
-    box-shadow:1px 1px 5px #6F6666;
+  button:hover {
+    box-shadow: 1px 1px 5px #6F6666;
   }
   
 </style>
+</head>
 <body>
 
 <?php
 
-	session_start();
-	
-	$msg = "Algo não correu bem!!! Dirija-se para a Página de Login ou Registe-se";
-	$btn = "Página Login";
-	$dir = "index.php";
-	
-	if(isset($_SESSION["erro"]))
-		$msg = $_SESSION["erro"];
-	if(isset($_SESSION["bt"]))
-		$btn = $_SESSION["bt"];
-	if(isset($_SESSION["dir"]))
-		$dir = $_SESSION["dir"];
-	
-	echo "
-	<div id='erro-box'>
-		<div id='erro-cabecalho'>Lamentamos...</div>
- 
-		<div class='input-div' id='input-user'>
-			$msg
-		</div> 
-  <!--=====================Login=====================-->
-		<form action='".$dir."'>
-			<div id='acoes'>
-				<input type='submit' value ='".$btn."'>
-				<div id = 'registo'><a href='./Registo/registo.php'>Registe-se...</a></div>
-			</div>
-		</form>";
-		
-	session_destroy();
-?>
-    </div>
+  session_start();
   
-</body>	
+  $msg = "Algo não correu bem!!! Dirija-se para a Página de Login ou Registe-se";
+  $btn = "Página Login";
+  $dir = "index.php";
+  
+  if(isset($_SESSION["erro"]))
+    $msg = $_SESSION["erro"];
+  if(isset($_SESSION["bt"]))
+    $btn = $_SESSION["bt"];
+  if(isset($_SESSION["dir"]))
+    $dir = $_SESSION["dir"];
+  
+  echo "
+  <div id='erro-box'>
+    <div id='erro-cabecalho'>Lamentamos...</div>
+ 
+    <div class='mensagem'>
+      $msg
+    </div> 
+  <!--=====================Login=====================-->
+    <form action='".$dir."'>
+      <div id='acoes'>
+        <div id='registo'><button type='submit' formaction='./PgRegisto.php'>Registe-se...</button></div>
+        <button type='submit'>$btn</button>
+      </div>
+    </form>";
+    
+  session_destroy();
+?>
+</div>
+  
+</body> 
 </html>
-
