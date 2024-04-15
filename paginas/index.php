c252c188e7dfc8cbb62873b897bb6cdd1bdf93ff
Guilherme Pires <79325677+Guilhermezanoello@users.noreply.github.com>
2024-04-15 12:22:19 +0100

    Novo Index.php

--- paginas/index.php
+++ paginas/index.php
@@ -1,65 +1,184 @@
 <!DOCTYPE html>
-<html lang="pt-br">
-
+<html>
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>Home - FormEST</title>
-    <meta name="description" content="O teu caminho para o conhecimento começa aqui - Bem-vindo ao FormEST">
-    <link rel="icon" type="image/png" sizes="499x251" href="logotipo.png">
+  <meta charset="utf-8">
+  <title>Web Site de Cursos de Formação</title>
 </head>
-
-<body style="background: linear-gradient(rgba(47, 23, 15, 0.65), rgba(47, 23, 15, 0.65)), url('cat-wash-23775237.jpg');">
-    <h1 class="text-center text-white d-none d-lg-block site-heading"><span class="text-primary site-heading-upper mb-3">o SALAO DE BELEZA P2ARA O SEU PET&nbsp;</span><span class="site-heading-lower">FormEST</span></h1>
-    <nav class="navbar navbar-dark navbar-expand-lg bg-dark py-lg-4" id="mainNav">
-        <div class="container"><a class="navbar-brand text-uppercase d-lg-none text-expanded" href="#">FormEST</a><button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navbarResponsive"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
-            <div class="collapse navbar-collapse" id="navbarResponsive">
-                <ul class="navbar-nav mx-auto">
-                    <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
-                    <li class="nav-item"><a class="nav-link" href="servicos.php">Serviços</a></li>
-                    <li class="nav-item"><a class="nav-link" href="horario.php">Horario</a></li>
-                    <?php
-                        session_start();
-
-                        if(isset($_SESSION['utilizador'])){
-                          switch ($_SESSION['tipo_user']){
-                              case 1:
-                                  echo '<li class="nav-item"><a class="nav-link" href="menu_cliente.php">Menu</a></li>';
-                                  break;
-                              case 2:
-                                  echo '<li class="nav-item"><a class="nav-link" href="menu_funcionario.php">Menu</a></li>';
-                                  break;
-                              case 3:
-                                  echo '<li class="nav-item"><a class="nav-link" href="menu_admin.php">Menu</a></li>';
-                                  break;
-                          }
-                            echo '<li class="nav-item"><a class="nav-link" href="logout.php">Logout</a></li>';
-                        } else {
-                           echo '<li class="nav-item"><a class="nav-link" href="login.php">Login</a></li>';
-                        }
-                    ?>
-                </ul>
-            </div>
-        </div>
-    </nav>
-    <section class="page-section clearfix"></section>
-    <section class="page-section cta">
-        <div class="container">
-            <div class="row">
-                <div class="col-xl-9 mx-auto">
-                    <div class="text-center cta-inner rounded">
-                        <h2 class="section-heading mb-4"><span class="section-heading-upper">A nossa Promessa</span><span class="section-heading-lower">PARA Si</span></h2>
-                        <p class="mb-0">When you walk into our shop to start your day, we are dedicated to providing you with friendly service, a welcoming atmosphere, and above all else, excellent products made with the highest quality ingredients. If you are not satisfied, please let us know and we will do whatever we can to make things right!</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-    <footer class="text-center footer text-faded py-5">
-        <div class="container">
-            <p class="m-0 small">Copyright&nbsp;©&nbsp;FormEST 2024</p>
-        </div>
-    </footer>
+<style>  
+  
+	body{
+		background-color: #f0f5f5; /* Azul claro */
+	}
+	#cabecalho{
+		margin: -8px;
+		height:200px;
+		background-image: url(./imgs/cabecalho.png);
+		border: 2px solid #1565c0; /* Azul escuro */
+	}
+	
+	.input-div{    
+		margin:25px;
+		float:right;
+		height:150px;
+	}
+  
+	input[type=submit]{
+   
+		background-color: #1565c0; /* Azul escuro */
+		padding:10px 20px;
+		height:50px;
+		font: bold 15px sans-serif;
+		color:white;
+		box-shadow:2px 2px 5px #888888; /* Cinza */
+		cursor:pointer;
+		border:0px;
+		border-radius: 25px; /* Arredondar os cantos */
+	}
+	
+	input[type=submit]:hover{
+		box-shadow:1px 1px 5px #888888; /* Cinza */
+	}
+	
+	#botoes{
+		margin:70px;
+	}
+  
+	#botao{
+		float:right;
+		margin:10px;
+	}
+  
+	#logo{
+		float:left;
+		background-image:url(./imgs/logo_tipo.png);
+		margin-left:80px;
+		margin-top:90px;
+		width:180px;
+		height:60px;
+	}
+	
+	a:link{
+		color:#1565c0; /* Azul escuro */
+		font: bold 15px sans-serif;
+		text-decoration:none;
+	}
+	a:visited{
+		color:#1565c0; /* Azul escuro */
+		font: bold 15px sans-serif;
+		text-decoration:none;
+	}
+	
+	#corpo{
+		width:1000px;
+		margin: 8px auto 0px;
+	}
+	#tabela{
+		width:1000px;
+		margin: 0px auto 0px;
+	}
+	#preco{
+		font: normal 15px sans-serif;
+		text-align: right;
+	}
+	
+	#titulo{
+		font: bold 15px sans-serif;
+		text-decoration:no;
+		text-align: left;
+	}
+	table, th, td {
+		border-collapse: collapse;
+	}
+	th, td {
+		padding: 15px 50px;
+	}
+	table#t01 tr:nth-child(even) {
+		background-color: #E3F2FD; /* Azul claro */
+	}
+	table#t01 tr:nth-child(odd) {
+		background-color: #BBDEFB; /* Azul médio */
+	}
+</style> 
+ 
+<body>  
+	<!-- GRAFISMO CABECALHO -->
+<div id="cabecalho">
+	<a href='pagina_inicial.php'>
+		<div id="logo">
+		</div>
+	</a>
+    <div class= "input-div">
+		<div id="botoes"> 
+			<?php
+				ob_start();
+				session_start();
+				
+				if(isset($_SESSION["user"])){
+					
+					$user = $_SESSION["user"];
+					unset($_SESSION);
+					$_SESSION["user"] = $user;
+					
+					echo "
+						<div id='botao'>
+							<form action='./logout.php'>
+								<input type='submit' value='Logout'>
+							</form>
+						</div>
+						<div id='botao'>
+							<form action='./PgUtilizador.php'>
+								<input type='submit' value='Area Pessoal'>
+							</form>
+						</div>
+					";	
+				}else {
+					
+					echo "
+						<div id='botao'>
+							<form action='./PgLogin.php'>
+								<input type='submit' value='Login'>
+							</form>
+						</div>
+						<div id='botao'> 
+							<form action='./PgRegisto.php'>
+								<input type='submit' value='Registe-se'>
+							</form>
+						</div>
+					";
+					
+				}
+			?>
+			<div id='botao'>
+				<form action="contatos.php">
+					<input type='submit' value='Contactos'>
+				</form>
+			</div>
+		</div>
+    </div>
+</div>
+  <!-- GRAFISMO CORPO -->
+<div id="corpo">
+	<div id="tabela">
+		<!-- <?php
+			include "./base_dados/basedados.h";
+			$sql = "SELECT nome,preco_p_noite,img_cabana,idCabana FROM cabana WHERE estado = '1'";
+			$retval = mysqli_query( $conn, $sql );
+			if(! $retval ){
+				die('Could not get data: ' . mysqli_error($conn));// se não funcionar dá erro
+			}
+			echo "<table width='100%' id = 't01'>";
+			while($row = mysqli_fetch_array($retval)){
+				echo"
+				<tr>
+					<td><a href='PgDescricao.php?IdCabana=".$row["idCabana"]."' ><img src='./imgs/".$row["img_cabana"]."'></a></td>
+					<td><a href='PgDescricao.php?IdCabana=".$row["idCabana"]."' ><div id='titulo'><b>".$row["nome"]."</b></div></a></td>
+					<td><div id='preco'>".$row["preco_p_noite"]." p/ noite</div></td>
+				</tr>"
+				;	
+			}
+			echo "</table>";
+		?>	 -->
+	</div>
+</div>
 </body>
-
 </html>
---
+++ paginas/index4.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Home - FormEST</title>
+    <meta name="description" content="O teu caminho para o conhecimento começa aqui - Bem-vindo ao FormEST">
+    <link rel="icon" type="image/png" sizes="499x251" href="logotipo.png">
+</head>
+
+<body style="background: linear-gradient(rgba(47, 23, 15, 0.65), rgba(47, 23, 15, 0.65)), url('cat-wash-23775237.jpg');">
+    <h1 class="text-center text-white d-none d-lg-block site-heading"><span class="text-primary site-heading-upper mb-3">o SALAO DE BELEZA P2ARA O SEU PET&nbsp;</span><span class="site-heading-lower">FormEST</span></h1>
+    <nav class="navbar navbar-dark navbar-expand-lg bg-dark py-lg-4" id="mainNav">
+        <div class="container"><a class="navbar-brand text-uppercase d-lg-none text-expanded" href="#">FormEST</a><button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navbarResponsive"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav mx-auto">
+                    <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
+                    <li class="nav-item"><a class="nav-link" href="servicos.php">Serviços</a></li>
+                    <li class="nav-item"><a class="nav-link" href="horario.php">Horario</a></li>
+                    <?php
+                        session_start();
+
+                        if(isset($_SESSION['utilizador'])){
+                          switch ($_SESSION['tipo_user']){
+                              case 1:
+                                  echo '<li class="nav-item"><a class="nav-link" href="menu_cliente.php">Menu</a></li>';
+                                  break;
+                              case 2:
+                                  echo '<li class="nav-item"><a class="nav-link" href="menu_funcionario.php">Menu</a></li>';
+                                  break;
+                              case 3:
+                                  echo '<li class="nav-item"><a class="nav-link" href="menu_admin.php">Menu</a></li>';
+                                  break;
+                          }
+                            echo '<li class="nav-item"><a class="nav-link" href="logout.php">Logout</a></li>';
+                        } else {
+                           echo '<li class="nav-item"><a class="nav-link" href="login.php">Login</a></li>';
+                        }
+                    ?>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <section class="page-section clearfix"></section>
+    <section class="page-section cta">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-9 mx-auto">
+                    <div class="text-center cta-inner rounded">
+                        <h2 class="section-heading mb-4"><span class="section-heading-upper">A nossa Promessa</span><span class="section-heading-lower">PARA Si</span></h2>
+                        <p class="mb-0">When you walk into our shop to start your day, we are dedicated to providing you with friendly service, a welcoming atmosphere, and above all else, excellent products made with the highest quality ingredients. If you are not satisfied, please let us know and we will do whatever we can to make things right!</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <footer class="text-center footer text-faded py-5">
+        <div class="container">
+            <p class="m-0 small">Copyright&nbsp;©&nbsp;FormEST 2024</p>
+        </div>
+    </footer>
+</body>
+
+</html>
